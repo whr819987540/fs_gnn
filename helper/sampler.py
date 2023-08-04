51f20ca3ca5c31c6dbe4a92d8f938eb175fcadd9
Gloriatry <845398616@qq.com>
2023-08-04 14:14:23 +0800

    合并本地fs分支修改；添加了几种采样函数

--- helper/sampler.py
+++ helper/sampler.py
@@ -44,41 +44,44 @@ class MyDataset(Dataset):
 #     shape = torch.Size(sparse_mx.shape)
 #     return indices, values, shape
 
-# #采样函数
-# def sampler(A, previous_nodes:list, sample_num:int):
-#     """
-#     A:torch.Tesor, 所有待选邻居节点（一个节点的所有邻居节点是包括它自己本身的）的邻接矩阵,
-#     行列数一样,对角线上都是1,即自己和自己连接
-#     previous_nodes: 上一层的节点在矩阵A中的index,而不是global id, 要求以在A中的ID从小到大的顺序排列
-#     sample_num:每层节点采样的最大值
-#     """
-#     U = A[previous_nodes,:]
-#     after_nodes = []
-#     for U_row in U:
-#         indices = U_row.indices
-#         sampled_indices = np.random.choice(indices, sample_num, replace=True)
-#         after_nodes.append(sampled_indices)
-#     after_nodes = np.unique(np.concatenate(after_nodes))
-#     # previous_nodes一定在after_nodes中
-#     after_nodes = np.concatenate([previous_nodes, np.setdiff1d(after_nodes, previous_nodes)])
-#     after_nodes = np.sort(after_nodes)
-#     adj = A[after_nodes, :][:, after_nodes]
-#     adj = normalize(adj)
-#     adj = adj[previous_nodes, :]
-#     #adj = row_normalize(adj)
-#     adj = sparse_mx_to_torch_sparse_tensor(adj) #将adj转换为sparse tensor类型,可以直接用于训练
-
-#     previous_index = np.where(np.isin(after_nodes, previous_nodes))[0]
-
-#     # 返回的adj用于前向传播中,after_nodes用于下一层采样,list,对应A中的ID,不是原ID
-#     # previous_index是previous_nodes在after_nodes中的索引,后面训练时要用
-#     return adj, after_nodes, previous_index
-
-
 def row_normalize(tensor):
     row_sum = tensor.sum(dim=1, keepdim=True) + 1e-20
     normalized_tensor = tensor / row_sum
     return normalized_tensor
+
+def normalize(adj):
+    rowsum = adj.sum(dim=1) + 1e-20
+    d_inv_sqrt = torch.pow(rowsum, -0.5)
+    d_inv_sqrt[torch.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = torch.diag(d_inv_sqrt)
+    adj = adj.to(d_mat_inv_sqrt.dtype)
+    adj_normalized = adj.matmul(d_mat_inv_sqrt).transpose(0, 1).matmul(d_mat_inv_sqrt)
+    return adj_normalized
+
+# 节点采样
+def node_wise_sampler(A, previous_nodes:list, sample_num:int):
+    """
+    A:torch.Tesor, 所有待选邻居节点（一个节点的所有邻居节点是包括它自己本身的）的邻接矩阵,
+    行列数一样,对角线上都是1,即自己和自己连接
+    previous_nodes: 上一层的节点在矩阵A中的index,而不是global id, 要求以在A中的ID从小到大的顺序排列
+    sample_num:每个节点采样的节点数
+    """
+    U = A[previous_nodes,:]
+    sampled_nodes = []
+    for U_row in U:
+        indices = U_row.nonzero().flatten()
+        sampled_indices = indices[torch.randperm(indices.shape[0])[:sample_num]]
+        sampled_nodes.append(sampled_indices)
+    sampled_nodes = torch.unique(torch.cat(sampled_nodes))
+    sampled_nodes = torch.unique(torch.cat([torch.tensor(previous_nodes), sampled_nodes]), sorted=True)
+    adj = U[:, sampled_nodes]
+    adj = row_normalize(adj)
+
+    previous_index = torch.where(torch.isin(sampled_nodes, torch.tensor(previous_nodes)))[0]
+
+    # 返回的adj用于前向传播中,sampled_nodes用于下一层采样,list,对应A中的ID,不是原ID
+    # previous_index是previous_nodes在after_nodes中的索引,后面训练时要用
+    return adj, sampled_nodes.tolist(), previous_index.tolist()
 
 # 层采样
 def layer_wise_sampling(A:torch.Tensor,previous_nodes:List[int],sample_num):
@@ -100,6 +103,29 @@ def layer_wise_sampling(A:torch.Tensor,previous_nodes:List[int],sample_num):
     # previous_index = torch.where(torch.isin(sampled_nodes, torch.tensor(previous_nodes)))[0]
 
     return adj, sampled_nodes
+
+# 层重要性采样
+def layer_importance_sampler(A, previous_nodes:list, sample_num:int):
+    '''
+    A:torch.Tesor, 所有待选邻居节点（一个节点的所有邻居节点是包括它自己本身的）的邻接矩阵,
+    行列数一样,对角线上都是1,即自己和自己连接
+    previous_nodes: 上一层的节点在矩阵A中的index,而不是global id, 要求以在A中的ID从小到大的顺序排列
+    sample_num:每层节点采样的最大值
+    '''
+    lap = normalize(A)
+    lap_sq = torch.mul(lap, lap)
+    pi = torch.sum(lap_sq[previous_nodes, :], dim=0)
+    p = pi / torch.sum(pi)
+    s_num = min(A.shape[0], sample_num)
+    sampled_nodes = torch.multinomial(p, s_num, replacement=False)
+    sampled_nodes = torch.sort(sampled_nodes)[0]
+    adj = lap[previous_nodes, :][:, sampled_nodes]
+    adj = torch.mul(adj, 1/p[sampled_nodes])
+    adj = row_normalize(adj)
+
+    # 返回的adj用于前向传播中,tensor.
+    # sampled_nodes用于下一层采样,list,对应A中的ID,不是global ID
+    return adj, sampled_nodes.tolist()
 
 if __name__ == "__main__":
     data = None
--- module/fs_layer.py
+++ module/fs_layer.py
@@ -1,8 +1,9 @@
 from torch import nn
 import torch
+import math
 
 class FSLayer(nn.Module):
-    def __init__(self, dim, weights, random=True, pretrain=True):
+    def __init__(self, dim, weights:torch.Tensor, random=True, pretrain=True):
         #dim为这一层的维度，输入维度输出维度一样, weights是训练参数初始值，用continous_feature_importance_gini函数算,
         # random为True表示训练参数weights随机初始化，为False表示用continous_feature_importance_gini函数初始化
 
@@ -15,9 +16,11 @@ class FSLayer(nn.Module):
     def reset_parameters(self, weights, random):   #初始化训练参数
         with torch.no_grad():
             if random:
-                torch.nn.init.uniform_(self.weights, a=0, b=1)
+                # torch.nn.init.uniform_(self.weights, a=0, b=1)
+                stdv = 1. / math.sqrt(self.weights.size(0))
+                self.weights.data.uniform_(-stdv, stdv)
             else:
-                self.weights.data = weights
+                self.weights.data.copy_(weights)
 
     def forward(self, x):
         s = torch.sigmoid(self.weights)
